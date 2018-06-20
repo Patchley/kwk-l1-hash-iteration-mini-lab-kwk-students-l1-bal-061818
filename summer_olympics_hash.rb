dbff7f0ad9ea291923f161e86c92fade9115e6fc
Patricia <patchley02@gmail.com>
2018-06-20 14:00:19 +0000

    automatically backed up by learn

--- summer_olympics_hash.rb
+++ summer_olympics_hash.rb
@@ -3,8 +3,7 @@ def create_olympics_hash
   {Sydney: "2000",
   Athens: "2004",
 Beijing: "2008",
-London: "2012"
-}# Implement this method so that it returns a hash with the data provided on README.md
+London: "2012"}# Implement this method so that it returns a hash with the data provided on README.md
 end
 
 def add_a_key_value_pair
